68b269e7d394f3184bdd1d01c42f1a033c381303
eswaribala <parameswaribala@gmail.com>
2022-05-25 17:31:58 +0530

    updated

--- day3/models/Bank.go
+++ day3/models/Bank.go
@@ -13,12 +13,28 @@ type Bank struct {
 	BranchName string             `bson:"Branch_Name"`
 }
 
+type BankSorter []Bank
+
 type IBank interface {
 	Create()
 	Edit()
 	Delete()
 }
 
+//total no of elements
+func (bankSorter BankSorter) Len() int {
+	return len(bankSorter)
+}
+
+//swap
+func (bankSorter BankSorter) Swap(i, j int) {
+	bankSorter[i], bankSorter[j] = bankSorter[j], bankSorter[i]
+}
+
+//attribue/property
+func (bankSorter BankSorter) Less(i, j int) bool {
+	return bankSorter[i].BankName < bankSorter[j].BankName
+}
 func (bank *Bank) Create() {
 	var bankRequest dto.BankRequest = dto.BankRequest{
 		bank.ID, bank.IFSCCode, bank.BankName,
--- day3/utility/SortData.go
+++ day3/utility/SortData.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"math/rand"
+	"necws/day3/models"
+	"sort"
 )
 
 func main() {
@@ -10,8 +13,37 @@ func main() {
 	for i := 0; i < len(data); i++ {
 		data[i] = rand.Intn(100000)
 	}
-	for _, value := range data {
-		fmt.Printf("%d\n", value)
+	//	fmt.Println("Before Sorting.....")
+	/*
+		for _, value := range data {
+			fmt.Printf("%d\n", value)
+		}
+	*///sort the data
+	sort.Ints(data)
+	//fmt.Println("After Sorting.....")
+	/*
+		for _, value := range data {
+			fmt.Printf("%d\n", value)
+		}
+	*/
+	bank1 := models.Bank{primitive.ObjectID{}, "H00001",
+		"HSBC", "Mylapore"}
+	bank2 := models.Bank{primitive.ObjectID{}, "S00001",
+		"SBI", "Mylapore"}
+	bank3 := models.Bank{primitive.ObjectID{}, "A00001",
+		"AXIS", "Mylapore"}
+	bank4 := models.Bank{primitive.ObjectID{}, "I00001",
+		"ICICI", "Mylapore"}
+
+	var banksorters = make(models.BankSorter, 5)
+	banksorters[0] = bank1
+	banksorters[1] = bank2
+	banksorters[2] = bank3
+	banksorters[3] = bank4
+	sort.Sort(banksorters)
+
+	for _, value := range banksorters {
+		fmt.Printf("%v\n", value)
 	}
 
 }

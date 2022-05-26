9fd83f051ef3dea198bd712c9ebd9bbdff1b2547
eswaribala <parameswaribala@gmail.com>
2022-05-26 16:02:44 +0530

    committed

--- day4/ConcuurentClockServerDemo.go
+++ day4/ConcuurentClockServerDemo.go
@@ -9,23 +9,26 @@ import (
 
 func main() {
 	listen, err := net.Listen("tcp",
-		"localhost:4000")
-	if err != nil {
-		log.Println("Net TCP Server not running")
-	} else {
-		conn, error := listen.Accept()
-		if error == nil {
-			log.Println("Connection accepted")
-			go handleConn(conn)
+		"localhost:8000")
+	for {
+		if err != nil {
+			log.Println("Net TCP Server not running")
+			continue
+		} else {
+			conn, error := listen.Accept()
+			//fmt.Println("Server Ready.....@ tcp 4000")
+			if error == nil {
+				log.Println("Connection accepted")
+				go handleConn(conn)
+			}
 		}
 	}
-
 }
 
 func handleConn(conn net.Conn) {
 	defer conn.Close()
 	for {
-		_, err := io.WriteString(conn, time.Now().Format("15:04:05\n"))
+		_, err := io.WriteString(conn, time.Now().Format("14:20:20"))
 		if err != nil {
 			return // e.g., client disconnected
 		}

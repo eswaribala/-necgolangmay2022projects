4b6890332c2bf1bd734c6d5d2e423df7bff3db73
eswaribala <parameswaribala@gmail.com>
2022-05-26 14:45:07 +0530

    committed

--- day4/RaceConditionDemo.go
+++ day4/RaceConditionDemo.go
@@ -11,17 +11,26 @@ var accountBalance int = 5000
 func main() {
 	wg1.Add(2)
 	go deposit(10000)
-	go deposit(10000)
+	go viewBalance()
 	wg1.Wait()
 }
 
 func deposit(amount int) {
 	defer wg1.Done()
 	for i := 0; i < 10; i++ {
-		availableBalance := accountBalance
+		//writing
 		accountBalance += amount
-		fmt.Printf("Available Balance%d\t", availableBalance)
 		fmt.Printf("Account Balance %d\n", accountBalance)
+
+	}
+}
+
+func viewBalance() {
+	defer wg1.Done()
+	for i := 0; i < 10; i++ {
+		//reading
+		availableBalance := accountBalance
+		fmt.Printf("Available Balance%d\n", availableBalance)
 
 	}
 }
